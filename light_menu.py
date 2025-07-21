9e73d5fad83a4dfe101d4bf425f5f8ca82871272
Nostagalaxy <115511353+Nostagalaxy@users.noreply.github.com>
2025-07-20 22:07:06 -0500

    I don't even know what I did at this point.

--- light_menu.py
+++ light_menu.py
@@ -1,7 +1,7 @@
 from textual.screen import Screen
 from textual.containers import Grid, Vertical
 from textual.app import ComposeResult
-from textual.widgets import Button, Static, RadioButton
+from textual.widgets import Button, Static, RadioButton, RadioSet
 
 from als import Als
 
@@ -22,12 +22,17 @@ class LightMenu(Screen):
                 yield Static("Station : " + str(self.light.station_id))
                 yield Static('Light : ' + str(self.light.pos))
                 yield Static("OTS")
-                yield RadioButton("Light")
-                yield RadioButton("Transciever")
+                with RadioSet():
+                    yield RadioButton("Light")
+                    yield RadioButton("Transciever")
                 yield Button("Quit", id="quit")
             with Vertical(id='right_panel'):
                 yield Static('Right Side')
 
     def on_button_pressed(self, event : Button.Pressed) -> None:
         if event.button.id == "quit":
-            self.app.pop_screen()   
+            self.app.pop_screen()  
+
+    def on_radio_button_changed(self, event : RadioButton.Changed) -> None:
+        if event.radio_button:
+            self.log("Button Changed") 
