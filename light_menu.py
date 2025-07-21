cd0d02b1475a87a2a7044e2f68a12ea33acde166
Nostagalaxy <115511353+Nostagalaxy@users.noreply.github.com>
2025-07-21 16:41:50 -0500

    Made changes to the layout of light menu.

--- light_menu.py
+++ light_menu.py
@@ -1,7 +1,7 @@
 from textual.screen import Screen
-from textual.containers import Grid, Vertical
+from textual.containers import Grid, Vertical, Horizontal
 from textual.app import ComposeResult
-from textual.widgets import Button, DataTable, Static, Checkbox, Header, Footer
+from textual.widgets import Button, DataTable, Static, Checkbox, Header, Footer, Input
 
 from rich.panel import Panel
 from rich.text import Text
@@ -22,24 +22,55 @@ class LightMenu(Screen):
         yield Header(True, name="Light Menu")
         
         with Grid():
-            with Vertical(id="left_panel"):
+            with Vertical(id="info_panel", markup=True) as Info:
+                Info.border_title = "Info"
 
+                # Station : #
                 content = Text.from_markup("[b]Station[/b] : " + "[bold purple]" + str(self.light.station_id) + "[/bold purple]")
-                yield Static(content, classes="leftside_line")
+                yield Static(content, classes="line")
                 
-                content = Text.from_markup("[b]Light[/b] : " + "[bold purple]" + str(self.light.pos) + "[/bold purple]")
-                yield Static(content, classes="leftside_line")
+                # Light : #
+                content = Text.from_markup("[b]Light[/b]   : " + "[bold purple]" + str(self.light.pos) + "[/bold purple]")
+                yield Static(content, classes="line")
                 
+                # TODO
+                yield Static("Type    : ")
+
+                # Get color code and interpret into a string
+                if self.light.color == 'g':
+                    content = Text.from_markup("[b]Color[/b]   : " + "[bold green]Green[/bold green]")
+                elif self.light.color == 'r':
+                    content = Text.from_markup("[b]Color[/b]   : " + "[bold red]Red[/bold red]")
+                elif self.light.color == 'w':
+                    content = Text.from_markup("[b]Color[/b]   : " + "[bold white]White[/bold white]")
+                
+                yield Static(content, classes="line")
+
+                # TODO
+                yield Static("Loop    :")
+                yield Static("Address :")
+
+
+
+                yield Button("Quit", id="quit")
+
+            with Vertical(id='data_panel'):
+                yield DataTable()
+                yield Input()
+                with Horizontal():
+                    yield Button("Log")
+                    yield Button("Edit")
+                    yield Button("Delete")
+
+            with Vertical(name="Light Status", id="status_panel") as Status:
+                Status.border_title = "Status"
                 content = Text.from_markup("[bold red]OTS[/bold red]")
                 yield Static(content,classes="leftside_line")
                 
                 yield Checkbox("Light", id="lm_checkbox_light")
                 yield Checkbox("Transciever")
-                
-                yield Button("Quit", id="quit")
             
-            with Vertical(id='right_panel'):
-                yield DataTable()
+            
         
         yield Footer()
 
