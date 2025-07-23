b3ccbc68ecdc22acc85c42ac0fd0c5d584f7514e
Nostagalaxy <115511353+Nostagalaxy@users.noreply.github.com>
2025-07-22 21:41:46 -0500

    Initialized db for lights and stations.

--- db_init.py
+++ db_init.py
@@ -1,3 +1,16 @@
 import sqlite3
 
+class DatabaseInterface:
+    def __init__(self, db_name: str):
+        self.connection = sqlite3.connect(db_name)
+        self.cursor = self.connection.cursor()
+        #self.__create_tables()
 
+    def query(self, query: str, params: tuple = ()):
+        self.cursor.execute(query, params)
+
+    def commit(self):
+        self.connection.commit()
+
+    def close(self):
+        self.connection.close()
--- test.py
+++ test.py
@@ -3,10 +3,20 @@ from textual.widgets import Static
 from rich import print
 
 from als import Als
+from db_init import DatabaseInterface as DB
 
 def main():
-    als = Als()
-    print(als.get_light(22, 4))
+    db = DB("databases/als.db")
+
+    for i in range(11, 25):
+        for j in range(1, 6):
+            db.query("INSERT INTO lights (station_id, position, type, color, status) VALUES (?, ?, ?, ?, ?)", (i, j, "sb", "white", True))
+                
+                
+
+
+    db.commit()
+
 
 if __name__ == "__main__":
     main() 
